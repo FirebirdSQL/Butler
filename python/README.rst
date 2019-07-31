7c22f9168770fbf68589dc3564de89692e18f223
Pavel Cisar <pcisar@ibphoenix.cz>
2019-07-31 14:53:13 +0200

    Python package for protocol buffers - Initial import

---
+++ python/README.rst
@@ -0,0 +1,8 @@
+================================
+Firebird Butler Protocol Buffers
+================================
+
+This package contains Python modules for work with protocol buffers defined by various
+Firebird Butler specifications_.
+
+.. _specifications: https://firebird-butler.readthedocs.io/en/latest/specifications.html
---
+++ python/setup.py
@@ -0,0 +1,63 @@
+#coding:utf-8
+"""A setuptools based setup module for FirebirdButlerProtobuf package.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.rst'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='firebird-butler-protobuf',
+    version='0.1',
+    description='Firebird Butler protocol buffers',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/x-rst",
+    url='https://github.com/FirebirdSQL/Butler',
+    author='Pavel Císař',
+    author_email='pcisar@ibphoenix.cz',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS',
+
+        'Topic :: Software Development',
+        ],
+    keywords='Firebird Butler protobuf Saturnin SDK',
+    packages=find_packages(),  # Required
+#    zip_safe=True,
+    install_requires=['protobuf>=3.9.0'],
+    python_requires='>=3.6, <4',
+    test_suite='nose.collector',
+    data_files=[],
+    namespace_packages=[],
+    project_urls={
+        'Source': 'https://github.com/FirebirdSQL/Butler/python',
+        'Documentation': 'https://firebird-butler.readthedocs.io/en/latest/',
+        },
+    entry_points={}
+)
